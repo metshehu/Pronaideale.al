175bf29aaf9a9aeea7ba98d3d9730cfaa607f5a5
met <mettusha8>
2024-03-21 21:56:12 +0100

    testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
           <h1 className="text-3xl font-bold underline">
-      Hello world! helllo
+      Hello world! helllo no way bro
     </h1>
     <p className='text-3xl font-bold underline-offset-1'>hello met hi bennet ku je shkemb</p>
     </>
